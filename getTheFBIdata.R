d1afb7f36e5fd82cd2ac558ec68215e8e7b48e2d
Stuart Mashaal <stumash1@gmail.com>
2016-06-18 20:47:18 +0200

    create single table containing all desired fbi data (#2)

--- getTheFBIdata.R
+++ getTheFBIdata.R
@@ -3,18 +3,31 @@
 library(readr)
 library(stringr)
 library(rvest)
-library(RCurl)
-library(XML)
 # get all the state names from the state_names text file
-table.list <- list()#; i = 2
-for (i in 1:length(state.names)) {
+table.list <- list()
+for (i in (1:length(state.names))[-38]) { #exclude 38th entry (pennsylvania has different url)
   curr.state.url <- str_replace(url.template, "STATENAME", state.names[i])
-  curr.page <- getURL(curr.state.url)
-  curr.page <- htmlParse(curr.state.url)
+  curr.page <- read_html(curr.state.url)
   curr.nodes <- html_nodes(curr.page, "table")
   curr.table <- html_table(curr.nodes[[3]])
   curr.table$State <- state.names[i]
   table.list[[i]] <- curr.table
 }
-table.list[[2]]
-# save(list = ls(all.names = TRUE), file = "fbi_scrape_required_data.RData", envir = .GlobalEnv)
+# get pennsyivania data separately (WHY DOES THE FBI WEBSITE NOT HAVE CONSISTENT PAGE-NAMING?)
+curr.state.url <- "https://www.fbi.gov/about-us/cjis/ucr/crime-in-the-u.s/2013/crime-in-the-u.s.-2013/tables/table-8/table-8-state-cuts/table-8-pennsylvania"
+curr.page <- read_html(curr.state.url)
+curr.nodes <- html_nodes(curr.page, "table")
+curr.table <- html_table(curr.nodes[[3]])
+curr.table$State <- state.names[38] # "pennsylvania"
+table.list[[38]] <- curr.table
+# make the long table
+cn <- c("City", "Population", "Violent_crime", "Murder_and_non-neg_manslaughter", "Rape_reviseddef", "Rape_legacydef", "Robbery", "Aggravated_assualt", "Property_crime", "Burglary", "Larceny_theft", "Vehicle_theft", "Arson", "State")
+for (i in 1:50) {
+  colnames(table.list[[i]]) <- cn
+}
+fbi <- do.call(rbind, table.list)
+# remove clutter variables from global environment
+rm(curr.nodes, curr.page, curr.table, curr.state.url, url.template, i, cn, state.names, table.list)
+
+# save the resulting table to a unclean_table.RData
+save(fbi, file = "unclean_table.RData.RData")
