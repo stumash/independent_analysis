799253f7a50c957164605b8dc9dbfafc34f44fb0
Stuart Mashaal <stumash1@gmail.com>
2016-06-19 07:20:43 +0200

    more eda, made tables just by state, rmarkdown improved (#7)

--- eda.R
+++ eda.R
@@ -4,7 +4,21 @@ library(ggplot2)
 library(choroplethr)
 load("clean_table.RData")
 load("table_per_capita.RData")
+load("state_tables.RData")
 View(fbi)
 View(fbi.pc)
+View(states.data)
+View(states.data.pc)
 
 ### eda
+# correlation matrix
+correlations <- matrix(nrow = ncol(fbi) - 2, ncol = ncol(fbi) - 2)
+colnames(correlations) <- (colnames(fbi)[-(1:2)])
+rownames(correlations) <- colnames(correlations)
+for(i in 1:nrow(correlations)) {
+  for(j in 1:ncol(correlations)) {
+    correlations[i,j] <- cor(fbi[,i+2], fbi[,j+2])
+  }
+}
+# population correlated with everything else
+
--- toByStateTable.R
+++ toByStateTable.R
@@ -1,1 +1,23 @@
 ##### make a table of the same data summed up by state
+load("clean_table.RData")
+load("table_per_capita.RData")
+states.data <- data.frame(matrix(ncol = 11, nrow = 50))
+colnames(states.data) <- colnames(fbi)[-(1:2)]
+rownames(states.data) <- levels(fbi$State)
+j <- 1
+for (currstate in levels(fbi$State)) {
+  currstate.data <- fbi[fbi$State == currstate,3:13]
+  currstate.row <- c()
+  for (i in 1:11) {
+    currstate.row <- c(currstate.row, sum(currstate.data[,i]))
+  }
+  states.data[j,] <- currstate.row
+  j <- j + 1
+}
+states.data.pc <- states.data
+for(i in nrow(states.data.pc)) {
+  states.data.pc[i,-1] <- states.data.pc[i,-1]/states.data.pc[i,1]
+}
+states.data.pc
+save(states.data, states.data.pc, file = "state_tables.RData")
+rm(currstate, currstate.row, i, currstate.data, j)
