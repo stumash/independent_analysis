461fae0cec695b3c2804c5a817cea8fc05bbe205
Stuart Mashaal <stumash1@gmail.com>
2016-06-19 23:43:17 +0200

    modeling done (lm's only), rmarkdown remains (#10)

---
+++ modelling.R
@@ -0,0 +1,18 @@
+# linear regression model to predict violent crime per city from all other variables, excluding
+# variables representing specific kinds of violent crime
+cityviollm <- lm(Violent_crime ~ . - Rape - Robbery - Aggravated_assualt - `Murder_and_non-neg_manslaughter` - City - State, fbi)
+summary(cityviollm)
+
+# linear regression model to predict violent crime per state from all other variables, 
+# excluding variables representing specific types of violent crime
+stateviollm <- lm(Violent_crime ~ . - Rape - Robbery - Aggravated_assualt - `Murder_and_non-neg_manslaughter`, states.data)
+summary(stateviollm) # good performance using only population
+
+# linear regression model to predict violent crime per city per capita from all other
+# variables, exluding variables representing specific types of violent crime
+cityviollmpc <- lm(Violent_crime ~ . - Rape - Robbery - Aggravated_assualt - `Murder_and_non-neg_manslaughter` - City - State, fbi.pc)
+summary(cityviollmpc)
+
+# linear regression model to predict violent crime per capita from all other variables
+stateviollmpc <- lm(Violent_crime ~ . - Rape - Robbery - Aggravated_assualt - `Murder_and_non-neg_manslaughter`, states.data.pc)
+summary(stateviollmpc)
